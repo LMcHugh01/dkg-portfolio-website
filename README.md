78b041cab5dd602a18fbf0da272001aef0540e02
Louis McHugh <mclouis2001@gmail.com>
2025-09-17 08:30:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# DKG Development Portfolio
+
+Welcome to the **DKG Development Portfolio**, a React-based website showcasing my web development projects, skills, and services.
+
+## About
+I am a web developer building modern, responsive websites using React and related technologies. This portfolio demonstrates my work and capabilities.
+
+## Features
+- Built with **React**
+- Responsive design for all devices
+- Showcase of personal projects and skills
+- Interactive components and modern web practices
+
+## Technologies Used
+- React
+- JavaScript (ES6+)
+- HTML5 & CSS3
+
+## Getting Started
+
+### Prerequisites
+- Node.js (v14+ recommended)
+- npm
+
+### Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/yourusername/dkg-website.git
+
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="main-content">
+      <Navbar />
+      <section id="home">
+        <h1>Home Section</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
+      </section>
+      <section id="work">
+        <h1>Work Section</h1>
+      </section>
+        <section id="about">
+      <h1>About Section</h1>
+        </section>
+      <section id="contact">
+        <h1>Contact Section</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
