e87df9dbc2d05e8356e4bbba882877f392c69ea0
Louis McHugh <mclouis2001@gmail.com>
2025-09-17 15:00:56 +0200

    Updated home section and some basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,60 @@
-import React from "react";
+import './App.css'
 import Navbar from "./components/Navbar";
+import githubLogo from "./assets/github-logo.png"
+import linkedInLogo from "./assets/linkedIn-logo.png"
+import cssLogo from "./assets/skill-icons/css-icon.png"
+
 
 function App() {
   return (
     <div className="main-content">
       <Navbar />
-      <section id="home">
-        <h1>Home Section</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio delectus neque quidem repellendus facilis aspernatur tempora maxime totam! Odio alias natus molestiae facilis a molestias labore voluptates, eligendi animi accusamus.</p>
-      </section>
-      <section id="work">
-        <h1>Work Section</h1>
-      </section>
-        <section id="about">
-      <h1>About Section</h1>
-        </section>
-      <section id="contact">
-        <h1>Contact Section</h1>
-      </section>
+      <div id="home" className="section">
+        <div className="section-content">
+          <p className='brand-2'>DKG Development</p>
+          <h1>Transforming Ideas into <span className='brand'>Responsive Web Experiences</span></h1>
+          <h5>Hi! I'm <span className="name">Louis McHugh</span>, an aspiring Front End Developer based in the Netherlands with a passion for code.</h5>
+          <button  className="btn"
+      onClick={() => {
+        const workSection = document.getElementById("work");
+        workSection.scrollIntoView({ behavior: "smooth" });
+      }}>Here is some of my work</button>
+        </div>
+      </div>
+
+      <div id="about" className="section">
+        <div className="section-content">
+          <h1>About Section</h1>
+          <h2>Skills</h2>
+          <div>
+
+          </div>
+        </div>
+      </div>
+
+      <div id="work" className="section">
+        <div className="section-content">
+          <h1>Work Section</h1>
+        </div>
+      </div>
+
+      <div id="contact" className="section">
+        <div className="section-content">
+          <h1>Contact Section</h1>
+          <div className="socialMedia-links">
+          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className='social-link'>
+            <img src={githubLogo} alt="GitHub" />
+          </a>
+          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className='social-link'>
+            <img src={linkedInLogo} alt="" />
+          </a>
+          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className='social-link'>
+            <img src={cssLogo} alt="" />
+          </a>
+        </div>
+      </div>
+
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,16 @@
 import { Link } from 'react-router-dom'
 import "../style/Navbar.css";
-import logo from "../assets/DKGLogo.png"
-import githubLogo from "../assets/github-logo.png"
-import linkedInLogo from "../assets/linkedIn-logo.png"
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={logo} alt="" />
-      </div>
       <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
-        <li><a href="#work">Work</a></li>
         <li><a href="#about">About</a></li>
+        <li><a href="#work">Work</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-      <div className="socialMedia-links">
-        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className='social-link'>
-          <img src={githubLogo} alt="GitHub" />
-        </a>
-        <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className='social-link'>
-          <img src={linkedInLogo} alt="" />
-        </a>
-      </div>
     </nav>
   );
 };
