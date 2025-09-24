c47eea77171a499ac78b4f285d62ef1dc64e3fda
Louis McHugh <mclouis2001@gmail.com>
2025-09-24 14:02:49 +0200

    update, added planet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,17 @@ import { Link } from 'react-router-dom'
 import "../style/Navbar.css";
 import { ShineBorder } from "../components/ui/shine-border";
 
-
 const Navbar = () => {
   return (
-    <div className="relative  w-full overflow-hidden">
-      <nav className="navbar">
-        <ShineBorder shineColor={["#f0f", "#c0f", "#0ff"]}/>
-        <ul className="navbar-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#work">Work</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="navbar">
+      <ShineBorder shineColor={["#f0f", "#c0f", "#0ff"]}/>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,6 +11,8 @@ import TypingText from "@/components/TypingText.jsx";
 
 import "../style/TypingText.css"
 
+import planetImg from "../assets/images/planet.png"
+
 import { ShootingStars } from "@/components/ui/shadcn-io/shooting-stars/index.jsx";
 import { AuroraText } from "@/components/ui/aurora-text";
 
@@ -23,83 +25,93 @@ import { AuroraText } from "@/components/ui/aurora-text";
     useEffect(() => {
       const timeout = setTimeout(() => {
         setShowIntro(true);
-      }, 1500); 
+      }, 2000); 
       return () => clearTimeout(timeout);
     }, []);
 
   return (
-    <div className="main-content">
-      <Navbar />
-      <div id="home" className="section relative w-full h-screen bg-transparent overflow-hidden">
-        {/* Shooting Stars Background */}
-        <ShootingStars
-          className="absolute inset-0 pointer-events-none z-0"
-          starColor="#9E00FF"
-          trailColor="#2EB9DF"
-          minSpeed={15}
-          maxSpeed={35}
-          minDelay={1200}
-          maxDelay={4200}
-        />
-        {/* Content Overlay */}
-        <div className="section-content relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4">
-          <div className={`intro transition-all duration-1000 ease-out transform ${showIntro ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
-            <p className="brand-2">DKG Development</p>
-            <h1>Hi there! I'm <AuroraText>Louis McHugh</AuroraText>.</h1>
+    <div className="all-content">
+      <div className="planet-container">
+        <img src={planetImg} alt="Planet" className="planet" />
+
+      </div>
+      <div className="main-content">
+        <Navbar />
+        <div id="home" className="section ">
+          
+          {/* Shooting Stars Background */}
+          <ShootingStars
+            className="absolute inset-0 pointer-events-none z-0"
+            starColor="#9E00FF"
+            trailColor="#2EB9DF"
+            minSpeed={15}
+            maxSpeed={35}
+            minDelay={1200}
+            maxDelay={4200}
+          />
+
+          {/* Content Overlay */}
+          <div className="section-content relative z-10 flex flex-col items-center justify-center text-white text-center px-4">         
+            <div className={`intro transition-all duration-1000 ease-out transform ${showIntro ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
+              <p className="brand-2">DKG Development</p>
+              <h1>Hi there! I'm <AuroraText>Louis McHugh</AuroraText></h1>
+            </div>
+            <span className="glow-line"></span>
+            <div className="flex flex-col items-center gap-8">
+              <h5 className="mt-4">
+                <span className="typing-cursor">
+                  <TypingText texts={["I'm an Aspiring Front End Developer", "I Love React & Tailwind", "Let's Build Something Awesome!"]} />
+                </span>
+              </h5>
+              <h6 className="location">📍Rotterdam, Netherlands.</h6>
+              <button
+                className="contact-btn btn mt-4"
+                onClick={() => {
+                  const contactSection = document.getElementById("contact");
+                  contactSection.scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                Get in touch ⌯⌲
+              </button>
+            </div>
           </div>
-          <span className="glow-line"></span>
-          <h5 className="mt-4">
-            <span className="typing-cursor">
-              <TypingText texts={["I'm an Aspiring Front End Developer", "I Love React & Tailwind", "Let's Build Something Awesome!"]} />
-            </span>
-          </h5>
-          <h6>📍Rotterdam, Netherlands.</h6>
-          <button
-            className="btn mt-4"
-            onClick={() => {
-              const contactSection = document.getElementById("contact");
-              contactSection.scrollIntoView({ behavior: "smooth" });
-            }}
-          >
-            Get in touch ⌯⌲
-          </button>
         </div>
-      </div>
 
-      {/* About Section */}
-      <div id="about" className="section">
-        <div className="section-content">
-          <h5>More about me...</h5>
-          <h3>
-            Meet <AuroraText>Louis</AuroraText>,
-          </h3>
-          <p>
-            I’m a passionate and detail-oriented web developer with a focus on
-            creating responsive, user-friendly digital experiences. My journey
-            into tech started with curiosity and quickly grew into a love for
-            building functional and visually appealing websites. With a
-            background that combines creativity and problem-solving, I enjoy
-            turning complex ideas into seamless solutions. Outside of coding, I’m
-            constantly exploring new tools and technologies to grow my skills and
-            stay ahead in this ever-evolving field.
-          </p>
-          <Skills />
+        {/* About Section */}
+        <div id="about" className="section">
+          <div className="section-content">
+            <h5>More about me...</h5>
+            <h3>
+              Meet <AuroraText>Louis</AuroraText>,
+            </h3>
+            <p>
+              I’m a passionate and detail-oriented web developer with a focus on
+              creating responsive, user-friendly digital experiences. My journey
+              into tech started with curiosity and quickly grew into a love for
+              building functional and visually appealing websites. With a
+              background that combines creativity and problem-solving, I enjoy
+              turning complex ideas into seamless solutions. Outside of coding, I’m
+              constantly exploring new tools and technologies to grow my skills and
+              stay ahead in this ever-evolving field.
+            </p>
+            <Skills />
 
+          </div>
         </div>
-      </div>
 
-      {/* Work Section */}
-      <div id="work" className="section">
-        <div className="section-content">
-          <Projects />
+        {/* Work Section */}
+        <div id="work" className="section">
+          <div className="section-content">
+            <Projects />
+          </div>
         </div>
-      </div>
 
-      {/* Contact Section */}
-      <div id="contact" className="section">
-        <div className="section-content">
-          <h1>Contact Section</h1>
-          <Contact />
+        {/* Contact Section */}
+        <div id="contact" className="section">
+          <div className="section-content">
+            <h1>Contact Section</h1>
+            <Contact />
+          </div>
         </div>
       </div>
     </div>
